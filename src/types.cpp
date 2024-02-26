560dd67f30ecbad4ecf428f537a3c8aadf8e5525
ImSwampy <83744625+ImSwampy@users.noreply.github.com>
2024-02-26 18:09:58 +0100

    types

---
+++ src/types.cpp
@@ -0,0 +1,27 @@
+#define L_PAR '('
+#define R_PAR ')'
+#define L_SQR_BRAQ '{'
+#define R_SQR_BRAQ '}'
+#define COLON ':'
+#define SEMI_COLON ';'
+#define QUOTE '\''
+#define DOUBLE_QUOTE '"'
+#define NUMS {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+#define LETTERS {'a', 'b', 'c', 'd', 'e', 'f', 'g', \
+                'h', 'i', 'j', 'k', 'l', 'm', 'n', \
+                'o', 'p', 'q', 'r', 's', 't', 'u', \
+                'v', 'w', 'x', 'y', 'z'}
+
+#define UPPER_LETTERS {'A', 'B', 'C', 'D', 'E', 'F', 'G', \
+                       'H', 'I', 'J', 'K', 'L', 'M', 'N', \
+                       'O', 'P', 'Q', 'R', 'S', 'T', 'U', \
+                       'V', 'W', 'X', 'Y', 'Z'}
+
+#define HASHTAG '#'
+#define DECORATOR '@'
+#define SPACE ' '
+#define TAB '\t'
+#define ENDL '\n'
+#define EQUAL '='
+#define DOUBLE_EQUAL '=='
+#define NOT '!'
