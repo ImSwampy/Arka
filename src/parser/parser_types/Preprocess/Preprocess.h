755d94622ddebabb184acf3c6b60a56281934c1e
Swampy <tijaguar44@gmail.com>
2024-03-07 11:59:36 +0100

    switching to testing branch, bugs included

--- src/parser/parser_types/Preprocess/Preprocess.h
+++ src/parser/parser_types/Preprocess/Preprocess.h
@@ -4,7 +4,16 @@
 #include <parser/Parser.h>
 
 class Preprocess {
-    Preprocess(Type::Preprocess::type , )
+public:
+    Preprocess(Type::Preprocess type) : m_type(type){};
+    Type::Preprocess m_type;
+    
+    std::string type = m_type.type;
+    int args_size = m_type.args->size();
+    std::string *args = new std::string[args_size];
+    
+private:
+
 };
 
 #endif // ARKA_PARSER_PREPROCESS_H
